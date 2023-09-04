3d6f63163c60981f2e20bac69410b5e1b5c6143f
yo-livy <yo.livy7@gmail.com>
2023-09-04 10:54:57 +0300

    find index

---
+++ find_index_of_first_occurance.js
@@ -0,0 +1,7 @@
+const findIndex = (haystack, needle) => {
+    return haystack.indexOf(needle);
+}
+
+console.log(findIndex('hello', 'e'));
+console.log(findIndex('snjkehfw;dje', 'keh'));
+console.log(findIndex('adskfjbadsfk', 'xc'));
