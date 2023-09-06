f715036f4a528ba773e1774248e5ead73c7ee9ab
yo-livy <yo.livy7@gmail.com>
2023-09-06 21:55:55 +0300

    baseball game

---
+++ baseball_game.js
@@ -0,0 +1,19 @@
+const game = (operations) => {
+    let stack = [];
+    for(op of operations){
+        if(op === '+'){
+            const score1 = stack.pop();
+            const score2 = stack.pop();
+            stack.push(score2, score1, score2 + score1); 
+        }else if(op === 'D'){
+            stack.push(2 * stack[stack.length - 1]);
+        }else if(op === 'C'){
+            stack.pop();
+        }else{
+            stack.push(parseInt(op));
+        }
+    }
+    return stack.reduce((a,b) => a + b, 0);
+}
+
+console.log(game(["5","2","C","D","+"]));
